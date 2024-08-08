eeee34082b1e3035d9ddd56702b2b99c86ef2644
haritestaccount <hariclashofclans7@gmail.com>
2024-08-08 19:06:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("7226386696:AAFl6-dy51k1HDQcqEgi77nchgKocjbk2K4")
-    APP_ID = int(os.environ.get("28410303"))
-    API_HASH = os.environ.get("fa71c1f1587432d034796c64c6660bf8")
-    CHANNEL_ID = int(os.environ.get("-1002001880594"))
+    TG_BOT_TOKEN = 7226386696:AAFl6-dy51k1HDQcqEgi77nchgKocjbk2K4
+    APP_ID = 28410303
+    API_HASH = fa71c1f1587432d034796c64c6660bf8
+    CHANNEL_ID = -1002001880594
